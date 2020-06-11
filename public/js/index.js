014a5b434bed0552f5ee1c55376ff6f768e96414
Gregory2911 <agnan.gregory@orange.fr>
2020-06-11 17:24:28 +0200

    last work of homepage's template

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,16 @@
-const nbrTricksLoaded = 5;
+// var nbrTricksLoaded = 5;
 var limit = 5;
 var offset = 0;
 
 function loadMoreTricks(limit, offset) {
     $.get("/tricks/" + limit + "/" + offset, function (data) {
         $("#arrow").hide();
+        alert(limit);
+        alert(offset);
         if(limit == 5)
         {
-            $("section#main").append(data);
+            $("html").append(data);
+            
         }
         else
         {
@@ -28,24 +31,32 @@ jQuery(document).ready(function () {
 
     $("#loadFirstTricks, #loadMoreTricks").on("click", function (e) {
         loadMoreTricks(limit, offset);
-        limit += nbrTricksLoaded;
-        offset += nbrTricksLoaded;
+        limit += 5;
+        offset += 5;
+        alert(limit);
+        alert(offset);
     });
+
+    // $("#loadMoreTricks").on("click", function (e) {
+    //     loadMoreTricks(limit, offset);
+    //     limit += nbrTricksLoaded;
+    //     offset += nbrTricksLoaded;
+    // });
 
     // Add smooth scrolling on links
-    $("#navbarSupportedContent #slideToTricksLink, #arrow a, #arrow2 a").on("click", function(event) {
+    // $("#navbarSupportedContent #slideToTricksLink, #arrow a, #arrow2 a").on("click", function(event) {
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            event.preventDefault(); // Prevent default anchor click behavior
-            var hash = this.hash; // Store hash
+    //     // Make sure this.hash has a value before overriding default behavior
+    //     if (this.hash !== "") {
+    //         event.preventDefault(); // Prevent default anchor click behavior
+    //         var hash = this.hash; // Store hash
 
-            // smooth page autoscroll taking specified milliseconds to scroll to the specified area
-            $("html, body").animate({
-            scrollTop: $(hash).offset().top
-            }, 800, function(){
-            window.location.hash = hash; // Add hash (#) to URL when done scrolling (default click behavior)
-            });
-        }
-    });
+    //         // smooth page autoscroll taking specified milliseconds to scroll to the specified area
+    //         $("html, body").animate({
+    //         scrollTop: $(hash).offset().top
+    //         }, 800, function(){
+    //         window.location.hash = hash; // Add hash (#) to URL when done scrolling (default click behavior)
+    //         });
+    //     }
+    // });
 });
