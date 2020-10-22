20778690e13bdc9f79a72106ccfd2b4d5fd2a4af
Gregory2911 <agnan.gregory@orange.fr>
2020-10-22 17:14:21 +0200

    last work on the password forgetting procedure

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,6 +7,7 @@ use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -50,5 +51,46 @@ class SecurityController extends AbstractController
      * @Route("/deconnexion", name="security_logout")
     */
     public function logout(){}
+
+    /**
+     * @Route("/forgot_password", name="security_forgot_password")
+     */
+    public function forgotPassword(Request $request, EntityManagerInterface $manager){
+
+        $form = $this->createFormBuilder()
+                     ->add('username',TextType::class)
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $username = $form->getData()['username'];
+
+            // $repo = $this->getDoctrine()->getRepository(User::class);
+
+            // $user = $repo->findOneBy([
+            //     'username' => $username
+            // ]);
+
+            $user = $manager->getRepository(User::class)->findOneBy([
+                'username' => $username
+            ]);
+
+            if($user == false){
+                var_dump($user);
+                // die();
+                $this->addFlash('warning','Pseudo inconnu !');
+                return $this->render('security/forgot_password.html.twig',[
+                    'form' => $form->createView()
+                ]);
+            }
+            
+        }
+        else{
+            return $this->render('security/forgot_password.html.twig',[
+                'form' => $form->createView()
+            ]);
+        }
+    }
 
 }
